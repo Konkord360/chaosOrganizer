6143c5fa26a96af1477e22f983532ee17f079eab
Konrad Korzeniowski <konrad@pop-os.localdomain>
2022-06-27 00:47:14 +0200

    Fixed test for PaymentController

--- src/main/java/com/mailReminder/restservice/RestServiceApplication.java
+++ src/main/java/com/mailReminder/restservice/RestServiceApplication.java
@@ -2,6 +2,10 @@ package com.mailReminder.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+
+import static org.springframework.context.annotation.FilterType.CUSTOM;
 
 @SpringBootApplication
 public class RestServiceApplication {
--- src/test/java/com/mailReminder/restservice/controller/PaymentControllerTest.java
+++ src/test/java/com/mailReminder/restservice/controller/PaymentControllerTest.java
@@ -5,7 +5,6 @@ import com.mailReminder.restservice.model.Payment;
 import com.mailReminder.restservice.model.Role;
 import com.mailReminder.restservice.model.User;
 import com.mailReminder.restservice.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,9 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -32,101 +28,75 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 class PaymentControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private UserRepository userRepository;
 
+    @Test
+    void testGetPaymentsReturnsCorrectPayments() throws Exception {
+        var user = createUser();
+        var payment = createPayment();
+        user.setPayments(List.of(payment));
 
-    @BeforeEach
-    void init(){
+        when(userRepository.findByEmail(any())).thenReturn(user);
 
+        this.mockMvc.perform(get("/getPayments?userEmail=testemail@gmail.com")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*]", hasSize(1)))
+                .andExpect(jsonPath("$[0].paymentTitle", is(payment.getPaymentTitle())))
+                .andExpect(jsonPath("$[0].amountOfSinglePayment", is(payment.getAmountOfSinglePayment().doubleValue())))
+                .andExpect(jsonPath("$[0].wholeAmount", is(payment.getWholeAmount().doubleValue())))
+                .andExpect(jsonPath("$[0].deadline", is(payment.getDeadline())))
+                .andExpect(jsonPath("$[0].receiverIBAN", is(payment.getReceiverIBAN())))
+                .andExpect(jsonPath("$[0].receiverName", is(payment.getReceiverName())))
+                .andExpect(jsonPath("$[0].senderIBAN", is(payment.getSenderIBAN())))
+                .andExpect(jsonPath("$[0].payedByNow", is(payment.getPayedByNow().doubleValue())));
     }
 
-//    @Test
-//    void testGetPaymentsReturnsCorrectPayments() throws Exception {
-////        Mock userRopositoryMock = Mockito.mock(UserRepository.class)class;
-//        var user = createUser();
-//        var payment = createPayment();
-//        user.setPayments(List.of(payment));
-//
-//        when(userRepository.findByEmail(any())).thenReturn(user);
-//
-//        this.mockMvc.perform(get("/getPayments?userLogin=test")).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[*]", hasSize(1)))
-//                .andExpect(jsonPath("$[0].paymentTitle", is(payment.getPaymentTitle())))
-//                .andExpect(jsonPath("$[0].amountOfSinglePayment", is(payment.getAmountOfSinglePayment().doubleValue())))
-//                .andExpect(jsonPath("$[0].wholeAmount", is(payment.getWholeAmount().doubleValue())))
-//                .andExpect(jsonPath("$[0].deadline", is(payment.getDeadline())))
-//                .andExpect(jsonPath("$[0].receiverIBAN", is(payment.getReceiverIBAN())))
-//                .andExpect(jsonPath("$[0].receiverName", is(payment.getReceiverName())))
-//                .andExpect(jsonPath("$[0].senderIBAN", is(payment.getSenderIBAN())))
-//                .andExpect(jsonPath("$[0].payedByNow", is(payment.getPayedByNow().doubleValue())));
-//    }
-//
-//    @Test
-//    void testAddsProperly() throws Exception {
-//        var user = createUser();
-//        var payment = createPayment();
-////        user.setPayments(List.of(payment));
-//        when(userRepository.findByEmail(any())).thenReturn(user);
-//
-//        this.mockMvc.perform(post("/addPayment?ownerLogin=test").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
-//                .content(payment.toString())).andDo(print())
-//                //.content("{\"paymentTitle\":\"test\",\"amountOfSinglePayment\":100.0,\"wholeAmount\":10.0,\"deadline\":\"21/10/1998\",\"receiverIBAN\":\"testIBAN\",\"receiverName\":\"MrTest\",\"senderIBAN\":\"testIBAN\",\"payedByNow\":10.0}")).andDo(print())
-//                .andExpect(status().isOk());
-//        this.mockMvc.perform(get("/getPayments?userLogin=test")).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[*]", hasSize(1)));
-//    }
-//
-//    @Test
-//    void testDeletesProperly() throws Exception {
-//        var user = createUser();
-//        var payments = createPayments(3);
-//        user.setPayments(payments);
-//
-//        when(userRepository.findByEmail(any())).thenReturn(user);
-//
-//        this.mockMvc.perform(delete("/deletePayment?ownerLogin=test&paymentIndex=1").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[*]", hasSize(2)));
-//    }
-//
-//    @Test
-//    void testModifiesProperly() throws Exception {
-//        var user = createUser();
-//        var payments = createPayments(3);
-//        var updatedPayment = createPayment();
-//        user.setPayments(payments);
-//
-//        when(userRepository.findByEmail(any())).thenReturn(user);
-//
-//        this.mockMvc.perform(delete("/deletePayment?ownerLogin=test&paymentIndex=1").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[*]", hasSize(2)));
-//    }
-//
-//    @Test
-//    void testNewPaymentIsAddedWithDefaultReminder() throws Exception{
-//
-//        var user = createUser();
-//        var payment = createPayment();
-////        user.setPayments(List.of(payment));
-//        when(userRepository.findByEmail(any())).thenReturn(user);
-//
-//        this.mockMvc.perform(post("/addPayment?ownerLogin=test").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
-//                .content(payment.toString())).andDo(print())
-//                //.content("{\"paymentTitle\":\"test\",\"amountOfSinglePayment\":100.0,\"wholeAmount\":10.0,\"deadline\":\"21/10/1998\",\"receiverIBAN\":\"testIBAN\",\"receiverName\":\"MrTest\",\"senderIBAN\":\"testIBAN\",\"payedByNow\":10.0}")).andDo(print())
-//                .andExpect(status().isOk());
-//        this.mockMvc.perform(get("/getPayments?userLogin=test")).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[*]", hasSize(1)))
-//                .andExpect(jsonPath("$[0].reminder.date", is("1998-10-23")));
-//    }
+    @Test
+    void testAddsProperly() throws Exception {
+        var user = createUser();
+        var payment = createPayment();
+
+        when(userRepository.findByEmail(any())).thenReturn(user);
+        this.mockMvc.perform(post("/addPayment?userEmail=testemail@gmail.com").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
+                        .content(payment.toString())).andDo(print())
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/getPayments?userEmail=testemail@gmail.com")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*]", hasSize(1)));
+    }
+
+    @Test
+    void testDeletesProperly() throws Exception {
+        var user = createUser();
+        var payments = createPayments(3);
+        user.setPayments(payments);
+
+        when(userRepository.findByEmail(any())).thenReturn(user);
+
+        this.mockMvc.perform(delete("/deletePayment?userEmail=testemail@gmail.com&paymentIndex=1").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*]", hasSize(2)));
+    }
+
+    @Test
+    void testModifiesProperly() throws Exception {
+        var user = createUser();
+        var payments = createPayments(3);
+        var updatedPayment = createPayment();
+        user.setPayments(payments);
+
+        when(userRepository.findByEmail(any())).thenReturn(user);
+
+        this.mockMvc.perform(delete("/deletePayment?userEmail=testemail@gmail.com&paymentIndex=1").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*]", hasSize(2)));
+    }
+
 
     private User createUser(){
         User user = new User();
